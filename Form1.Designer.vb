5f3c428e795301065a647d4b36a1b9b4c3fba380
cesarantoniomello <79757763+cesarantoniomello@users.noreply.github.com>
2021-02-27 12:35:45 -0300

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.cmd_atualiza = New System.Windows.Forms.Button()
+        Me.pbar_atualiza = New System.Windows.Forms.ProgressBar()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txt_step = New System.Windows.Forms.MaskedTextBox()
+        Me.btn_mais = New System.Windows.Forms.Button()
+        Me.btn_menos = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cmd_atualiza
+        '
+        Me.cmd_atualiza.Location = New System.Drawing.Point(713, 63)
+        Me.cmd_atualiza.Name = "cmd_atualiza"
+        Me.cmd_atualiza.Size = New System.Drawing.Size(75, 23)
+        Me.cmd_atualiza.TabIndex = 0
+        Me.cmd_atualiza.Text = "Atualizar"
+        Me.cmd_atualiza.UseVisualStyleBackColor = True
+        '
+        'pbar_atualiza
+        '
+        Me.pbar_atualiza.Location = New System.Drawing.Point(12, 34)
+        Me.pbar_atualiza.Name = "pbar_atualiza"
+        Me.pbar_atualiza.Size = New System.Drawing.Size(776, 23)
+        Me.pbar_atualiza.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(388, 69)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(29, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Step"
+        '
+        'txt_step
+        '
+        Me.txt_step.Location = New System.Drawing.Point(423, 66)
+        Me.txt_step.Mask = "00000"
+        Me.txt_step.Name = "txt_step"
+        Me.txt_step.ReadOnly = True
+        Me.txt_step.Size = New System.Drawing.Size(100, 20)
+        Me.txt_step.TabIndex = 4
+        Me.txt_step.Text = "500"
+        Me.txt_step.ValidatingType = GetType(Integer)
+        '
+        'btn_mais
+        '
+        Me.btn_mais.Location = New System.Drawing.Point(564, 64)
+        Me.btn_mais.Name = "btn_mais"
+        Me.btn_mais.Size = New System.Drawing.Size(31, 23)
+        Me.btn_mais.TabIndex = 5
+        Me.btn_mais.Text = ">>"
+        Me.btn_mais.UseVisualStyleBackColor = True
+        '
+        'btn_menos
+        '
+        Me.btn_menos.Location = New System.Drawing.Point(529, 63)
+        Me.btn_menos.Name = "btn_menos"
+        Me.btn_menos.Size = New System.Drawing.Size(29, 23)
+        Me.btn_menos.TabIndex = 6
+        Me.btn_menos.Text = "<<"
+        Me.btn_menos.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 105)
+        Me.Controls.Add(Me.btn_menos)
+        Me.Controls.Add(Me.btn_mais)
+        Me.Controls.Add(Me.txt_step)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.pbar_atualiza)
+        Me.Controls.Add(Me.cmd_atualiza)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cmd_atualiza As Button
+    Friend WithEvents pbar_atualiza As ProgressBar
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txt_step As MaskedTextBox
+    Friend WithEvents btn_mais As Button
+    Friend WithEvents btn_menos As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,105 @@
+﻿
+Public Class Form1
+    Dim conn As ADODB.Connection
+    Private Sub cmd_atualiza_Click(sender As Object, e As EventArgs) Handles cmd_atualiza.Click
+        Me.Text = "Lendo Valores"
+
+
+        Dim textocompleto As String
+        Dim linhas
+        Dim linhatexto
+        Dim valores
+        Dim strcon
+        Dim idadeoriginal
+        Dim DATA_INICIO_SINTOMAS
+        Dim DATA_OBITO
+        Dim DATA_OBITO_DIVULGACAO
+        Dim DATA_RECUPERADO_DIVULGACAO
+        Dim strsql
+        Dim contador
+        conn = New ADODB.Connection
+        contador = 0
+        Me.Text = "Baixando Arquivo Decimal Dados"
+        Try
+            'My.Computer.FileSystem.DeleteFile("C:\temp\covid.csv")
+            My.Computer.Network.DownloadFile("https://www.saude.pr.gov.br/sites/default/arquivos_restritos/files/documento/2021-02/informe_epidemiologico_26_02_2021_geral.csv", "C:\Temp\covid2.csv")
+        Catch ex As Exception
+
+        End Try
+        Me.Text = "Lendo arquivo do HD"
+        Dim fileReader As System.IO.StreamReader
+        fileReader = My.Computer.FileSystem.OpenTextFileReader("C:\temp\covid2.csv", System.Text.Encoding.Default)
+
+        textocompleto = fileReader.ReadToEnd
+        strcon = "DRIVER=MySQL ODBC 3.51 Driver; SERVER=localhost;DATABASE=covid; UID=covid;PASSWORD=covid;OPTION=" & 1 + 2 + 8 + 32 + 2048 + 16384
+        conn.Open(strcon)
+        Me.Text = "Parsando arquivo na memoria"
+        linhas = Split(textocompleto, vbCrLf)
+        strsql = "INSERT INTO geral2 (`IBGE_RES_PR`,`IBGE_ATEND_PR`,`SEXO`,`IDADE_ORIGINAL`,`MUN_RESIDENCIA`,`MUN_ATENDIMENTO`,`LABORATORIO`,`DATA_DIAGNOSTICO`,`DATA_CONFIRMACAO_DIVULGACAO`,`DATA_INICIO_SINTOMAS`,`OBITO`,`DATA_OBITO`,`DATA_OBITO_DIVULGACAO`,`STATUS`,`DATA_RECUPERADO_DIVULGACAO`,`FONTE_DADO_RECUPERADO`) VALUES "
+        pbar_atualiza.Maximum = UBound(linhas) - 1
+        For i = 1 To UBound(linhas) - 1
+
+
+            linhatexto = linhas(i)
+            valores = Split(linhatexto, ";")
+            If IsNumeric(valores(3)) Then idadeoriginal = valores(3).ToString.Replace(Chr(34), "") Else idadeoriginal = ""
+            If IsDate(valores(9).ToString.Replace(Chr(34), "")) Then DATA_INICIO_SINTOMAS = "'" & Format(CDate(valores(9).ToString.Replace(Chr(34), "")), "yyyy-MM-dd") & "'" Else DATA_INICIO_SINTOMAS = "NULL"
+            If IsDate(valores(11).ToString.Replace(Chr(34), "")) Then DATA_OBITO = "'" & Format(CDate(valores(11).ToString.Replace(Chr(34), "")), "yyyy-MM-dd") & "'" Else DATA_OBITO = "NULL"
+            If IsDate(valores(12).ToString.Replace(Chr(34), "")) Then DATA_OBITO_DIVULGACAO = "'" & Format(CDate(valores(12).ToString.Replace(Chr(34), "")), "yyyy-MM-dd") & "'" Else DATA_OBITO_DIVULGACAO = "NULL"
+            If IsDate(valores(14).ToString.Replace(Chr(34), "")) Then DATA_RECUPERADO_DIVULGACAO = "'" & Format(CDate(valores(14).ToString.Replace(Chr(34), "")), "yyyy-MM-dd") & "'" Else DATA_RECUPERADO_DIVULGACAO = "NULL"
+
+
+            strsql = strsql _
+            & "(" & valores(0).ToString.Replace(Chr(34), "") _
+            & "," & valores(1).ToString.Replace(Chr(34), "") _
+            & ",'" & valores(2).ToString.Replace(Chr(34), "") & "'" _
+            & "," & idadeoriginal _
+            & ",'" & valores(4).ToString.Replace("'", "\'") & "'" _
+            & ",'" & valores(5).ToString.Replace("'", "\'") & "'" _
+            & ",'" & valores(6).ToString.Replace(Chr(34), "") & "'" _
+            & ",'" & Format(CDate(valores(7).ToString.Replace(Chr(34), "")), "yyyy-MM-dd") & "'" _
+            & ",'" & Format(CDate(valores(8).ToString.Replace(Chr(34), "")), "yyyy-MM-dd") & "'" _
+            & "," & DATA_INICIO_SINTOMAS _
+            & ",'" & valores(10).ToString.Replace(Chr(34), "") & "'" _
+            & "," & DATA_OBITO _
+            & "," & DATA_OBITO_DIVULGACAO _
+            & ",'" & valores(13).ToString.Replace(Chr(34), "") & "'" _
+            & "," & DATA_RECUPERADO_DIVULGACAO _
+            & ",'" & valores(15).ToString.Replace(Chr(34), "") & "')," & vbCrLf
+            contador = contador + 1
+            If contador > txt_step.Text.ToString Then
+                pbar_atualiza.Value = i
+                Me.Text = "Gravando Dados" & i & " de " & UBound(linhas)
+                strsql = Mid(strsql, 1, strsql.ToString.Length - 3)
+                strsql = strsql & ";"
+                conn.Execute(strsql)
+                contador = 0
+                strsql = "INSERT INTO geral2 (`IBGE_RES_PR`,`IBGE_ATEND_PR`,`SEXO`,`IDADE_ORIGINAL`,`MUN_RESIDENCIA`,`MUN_ATENDIMENTO`,`LABORATORIO`,`DATA_DIAGNOSTICO`,`DATA_CONFIRMACAO_DIVULGACAO`,`DATA_INICIO_SINTOMAS`,`OBITO`,`DATA_OBITO`,`DATA_OBITO_DIVULGACAO`,`STATUS`,`DATA_RECUPERADO_DIVULGACAO`,`FONTE_DADO_RECUPERADO`) VALUES "
+            End If
+
+            Application.DoEvents()
+
+        Next
+        strsql = Mid(strsql, 1, strsql.ToString.Length - 3)
+        strsql = strsql & ";"
+        conn.Execute(strsql)
+        pbar_atualiza.Value = pbar_atualiza.Value = pbar_atualiza.Maximum
+        Me.Text = "Gravando Dados" & pbar_atualiza.Value & " de " & pbar_atualiza.Maximum
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btn_menos_Click(sender As Object, e As EventArgs) Handles btn_menos.Click
+        txt_step.Text = txt_step.Text - 10
+    End Sub
+
+    Private Sub btn_mais_Click(sender As Object, e As EventArgs) Handles btn_mais.Click
+        txt_step.Text = txt_step.Text + 10
+    End Sub
+End Class
